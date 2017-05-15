f483e30d9fffddad16b4d45a154c61d0545567d3
BarkaBoss <bossmajor.biii@gmail.com>
2017-05-15 10:52:22 +0100

    first git project

--- src/cmp202/Example.java
+++ src/cmp202/Example.java
@@ -17,6 +17,8 @@ public class Example {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello world");
+        System.out.print("Hello world again");
     }
     
 }
